2f8c64ca80526f44006e6ac8f1b938ac9f4257f0
aakashbaranwal <baakash1998@gmail.com>
2020-07-08 15:15:31 +0530

    Updates

---
+++ precache-manifest.10eb6f53c49f9052bd01ec2f3315a542.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f80b483ccc204bf1663301d49ed4650b",
+    "url": "/reality/index.html"
+  },
+  {
+    "revision": "c31502381f9869aad36a",
+    "url": "/reality/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "ff3b763b819567d086d9",
+    "url": "/reality/static/css/main.3d5c4af7.chunk.css"
+  },
+  {
+    "revision": "c31502381f9869aad36a",
+    "url": "/reality/static/js/2.dd21a03e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/reality/static/js/2.dd21a03e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ff3b763b819567d086d9",
+    "url": "/reality/static/js/main.fc7f8d9b.chunk.js"
+  },
+  {
+    "revision": "30df111616b7f37b2ede",
+    "url": "/reality/static/js/runtime-main.21ce1714.js"
+  },
+  {
+    "revision": "186509e0d5036d8090f97568244676b7",
+    "url": "/reality/static/media/Aakash_Baranwal.186509e0.jpeg"
+  },
+  {
+    "revision": "35c35624092b1bf976cb890479bff014",
+    "url": "/reality/static/media/aakash_baranwal.35c35624.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/reality/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "6c086a3346d092a5c46b41af8de7a447",
+    "url": "/reality/static/media/me.6c086a33.png"
+  }
+]);
